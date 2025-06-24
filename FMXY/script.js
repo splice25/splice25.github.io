33d09b3fb866a1019bf92b9010e09857f76c825c
Sam Wells <32932600+sllewmas@users.noreply.github.com>
2025-06-24 16:36:50 -0400

    updated event handleing

--- FMXY/script.js
+++ FMXY/script.js
@@ -5,7 +5,7 @@ circle.style.display = "none";
 svg.appendChild(circle);
 
 let isTouching = false;
-let play, rnboX, rnboY
+
 
 function getNormalizedCoords(evt) {
     const rect = svg.getBoundingClientRect();
@@ -22,45 +22,9 @@ function updateCircle(x, y) {
     circle.style.display = "block";
 }
 
-function handleStart(evt) {
-    if(context.state === 'suspended'){
-        context.resume()
-    }
-    evt.preventDefault();
-    isTouching = true;
-    const { x, y, nx, ny } = getNormalizedCoords(evt);
-    updateCircle(x, y);
-    console.log({ x: nx.toFixed(3), y: ny.toFixed(3), touched: true });
-    play.value = 1;
-    rnboX.value = nx;
-    rnboY.value = ny;
-}
 
-function handleMove(evt) {
-    if (!isTouching) return;
-    const { x, y, nx, ny } = getNormalizedCoords(evt);
-    updateCircle(x, y);
-    console.log({ x: nx.toFixed(3), y: ny.toFixed(3), touched: true });
-    rnboX.value = nx;
-    rnboY.value = ny;
-}
 
-function handleEnd(evt) {
-    isTouching = false;
-    circle.style.display = "none";
-    console.log({ touched: false });
-    play.value = 0;
-}
 
-svg.addEventListener("pointerdown", handleStart);
-// svg.addEventListener("mousedown", handleStart);
-// svg.addEventListener("touchstart", handleStart);
-svg.addEventListener("pointermove", handleMove);
-// svg.addEventListener("mousemove", handleMove);
-// svg.addEventListener("touchmove", handleMove);
-svg.addEventListener("pointerup", handleEnd);
-// svg.addEventListener("mouseup", handleEnd);
-// svg.addEventListener("touchend", handleEnd);
 
 
 
@@ -90,15 +54,48 @@ const setup = async () => {
     let device = await RNBO.createDevice({ context, patcher });
 
     // Get a reference to the "play" parameter from the RNBO device
-    play = device.parametersById.get("play");
+    let play = device.parametersById.get("play");
 
-    rnboX = device.parametersById.get("x");
-    rnboY = device.parametersById.get("y");
+    let rnboX = device.parametersById.get("x");
+    let rnboY = device.parametersById.get("y");
 
     // Connect the RNBO device’s audio output to the gain node
     device.node.connect(outputNode);
 
     // Add an event listener for when the button is pressed down
+
+    svg.addEventListener("pointerdown", async (evt)=>{
+        const { x, y, nx, ny } = getNormalizedCoords(evt);
+        updateCircle(x, y);
+        isTouching = true;
+        if(context.state === 'suspended'){
+            await context.resume()
+        }
+        evt.preventDefault();
+
+        console.log("start~!");
+        play.value = 1;
+        rnboX.value = nx;
+        rnboY.value = ny;
+    });
+
+    svg.addEventListener("pointermove", (evt)=>{
+        if (!isTouching) return;
+        const { x, y, nx, ny } = getNormalizedCoords(evt);
+        updateCircle(x, y);
+        console.log({ x: nx.toFixed(3), y: ny.toFixed(3), touched: true });
+        rnboX.value = nx;
+        rnboY.value = ny;
+    });
+
+    svg.addEventListener("pointerup", (evt)=>{
+        isTouching = false;
+        circle.style.display = "none";
+        // console.log({ touched: false });
+        play.value = 0;
+    });
+
+
     // onoffCtl.addEventListener('input', async (event) => {
     //     // Resume the AudioContext if it's not already running (required by browsers)
     //     if (context.state === 'suspended') {
@@ -127,6 +124,8 @@ const setup = async () => {
     //     // Log "off" to the console for debugging
     //     console.log("off");
     // });
+
+
 };
 
 // Call the setup function to start everything
